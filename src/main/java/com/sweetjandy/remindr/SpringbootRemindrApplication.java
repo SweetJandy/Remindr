02749f6e1ec44724fecd6e9ae5bf3b0c9a203d10
brandongossen <brandongossen@yahoo.com>
2017-11-17 15:06:45 -0600

    Build A War From the application
    -Change the application to be buildable as a war.
    
    -Setup + Provision the server
    
    -Setup a database and site
    
    -Login to configure git deployment
    
    -Add the git remote to your project and push

--- src/main/java/com/sweetjandy/remindr/SpringbootRemindrApplication.java
+++ src/main/java/com/sweetjandy/remindr/SpringbootRemindrApplication.java
@@ -1,12 +1,17 @@
 package com.sweetjandy.remindr;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootRemindrApplication {
+public class SpringbootRemindrApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootRemindrApplication.class, args);
     }
-
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringbootRemindrApplication.class);
+    }
 }
