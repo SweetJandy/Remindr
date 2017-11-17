0e7aaf33c0c6ec289c6f0f33525d64c440c1b8c8
amyyan210 <amyyan210@gmail.com>
2017-11-17 14:00:11 -0600

    Start creating model for User class
    * Create properties and use hibernate to create columns for each
    * Create getters and setters for each property
    * Add dependencies to pom file
    * Create constructor

---
+++ src/main/java/com/sweetjandy/remindr/models/Users.java
@@ -0,0 +1,95 @@
+package com.sweetjandy.remindr.models;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
+
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    @NotBlank(message = "First name cannot be blank")
+    private String firstName;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Last name cannot be blank")
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    @NotBlank(message = "Phone number cannot be blank")
+    private String phoneNumber;
+
+    @Column(nullable = false, length = 50, unique = true)
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Email must have an @ symbol")
+    private String username;
+
+    @Column(nullable = false)
+    @JsonIgnore
+    @NotBlank(message = "Password cannot be blank")
+    private String password;
+
+
+    public User(long id, String firstName, String lastName, String phoneNumber, String username, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.username = username;
+        this.password = password;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
