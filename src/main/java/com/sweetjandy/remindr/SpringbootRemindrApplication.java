54b0c1deeb5c3283cca40b439a51e4e4366a9820
amyyan210 <amyyan210@gmail.com>
2017-11-17 11:12:16 -0600

    Work on build of project
    * Add thymeleaf dependency to POM
    * Add dev tools dependency to POM
    * Create main method
    * Add as maven project
    * Create hello world landing page and controller

---
+++ src/main/java/com/sweetjandy/remindr/SpringbootRemindrApplication.java
@@ -0,0 +1,12 @@
+package com.sweetjandy.remindr;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRemindrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRemindrApplication.class, args);
+    }
+
+}
