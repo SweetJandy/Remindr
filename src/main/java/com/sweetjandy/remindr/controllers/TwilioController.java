9e3e542bbff0602a46c393018c7db9d531a1a045
ironicsushi <yuli5565@colorado.edu>
2017-11-18 21:09:51 -0600

    * create generic response to any message sent to twilio number

--- src/main/java/com/sweetjandy/remindr/controllers/TwilioController.java
+++ src/main/java/com/sweetjandy/remindr/controllers/TwilioController.java
@@ -1,12 +1,16 @@
 package com.sweetjandy.remindr.controllers;
 import com.sweetjandy.remindr.services.TwilioService;
+import com.twilio.twiml.Body;
+import com.twilio.twiml.MessagingResponse;
 import org.springframework.stereotype.Controller;
 
 import com.twilio.type.PhoneNumber;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @Controller
@@ -22,8 +26,36 @@ public class TwilioController {
     @ResponseBody
     public String sendSMS () {
 
-        twilioSvc.sendASMS(new PhoneNumber(""), new PhoneNumber("+12104053232"), "https://cdn.pixabay.com/photo/2013/12/12/03/08/kitten-227009_960_720.jpg");
+        return twilioSvc.sendASMS(new PhoneNumber("+17203930339"), new PhoneNumber("+12104053232"), "https://cdn.pixabay.com/photo/2013/12/12/03/08/kitten-227009_960_720.jpg");
+    }
+
+    @RequestMapping(value ="/replySMS", produces = "text/xml")
+    @ResponseBody
+    public String replySMS() {
+        Body body = new Body("Welcome to Remindr!");
+
+        return twilioSvc.setResponse(body);
+
+    }
+
+    @RequestMapping(value ="/replySMS/{Body}", produces = "text/xml", method=RequestMethod.GET)
+    @ResponseBody
+    public String replySMS(
+       @RequestParam (value="Body", required=false) String body) {
+        System.out.println(body);
+
+//        Body optIn = new Body("You have opted in to the Remindr.");
+//        Body optOut = new Body("You have opted out of the Remindr.");
+//
+//        if (body.equalsIgnoreCase("yes")) {
+//            return twilioSvc.setResponse(optIn);
+//        } else if (body.equalsIgnoreCase("no")){
+//            return twilioSvc.setResponse(optOut);
+//        }
 
         return "";
+
     }
+
+
 }
--- src/main/java/com/sweetjandy/remindr/services/TwilioService.java
+++ src/main/java/com/sweetjandy/remindr/services/TwilioService.java
@@ -1,14 +1,19 @@
 package com.sweetjandy.remindr.services;
 
+
+//import com.twilio.twiml.Message;
+//import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.twiml.Body;
+import com.twilio.twiml.MessagingResponse;
+import com.twilio.twiml.TwiMLException;
+import com.twilio.Twilio;
+import com.twilio.type.PhoneNumber;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import com.twilio.Twilio;
-import com.twilio.rest.api.v2010.account.Message;
-import com.twilio.type.PhoneNumber;
-
+import javax.servlet.http.HttpServlet;
 
 @Service("twilioSvc")
-public class TwilioService {
+public class TwilioService extends HttpServlet {
 
     @Value("${twilio-account-sid}")
     private String twiliosid;
@@ -16,17 +21,40 @@ public class TwilioService {
     @Value("${twilio-auth-token}")
     private String twiliotoken;
 
-    public void sendASMS (PhoneNumber phoneNumberTo, PhoneNumber phoneNumberFrom, String mediaURL) {
+    public String sendASMS (PhoneNumber phoneNumberTo, PhoneNumber phoneNumberFrom, String mediaURL) {
         Twilio.init(this.twiliosid, this.twiliotoken);
 
-        Message message = Message
-                .creator(phoneNumberTo, phoneNumberFrom,
-                        "Hi Amy! It works!")
-                .setMediaUrl(mediaURL)
-                .create();
+        com.twilio.rest.api.v2010.account.Message message = com.twilio.rest.api.v2010.account.Message
+                    .creator(phoneNumberTo, phoneNumberFrom,
+                            "Hi Amy! It works!")
+                    .setMediaUrl(mediaURL)
+                    .create();
 
-        System.out.println(message.getSid());
+        return message.getSid();
 
+    }
+
+    public String setResponse (Body body) {
+        try {
+
+        com.twilio.twiml.Message message = new com.twilio.twiml.Message
+                .Builder()
+                .body(body)
+                .build();
+
+        MessagingResponse response = new MessagingResponse
+                .Builder()
+                .message(message)
+                .build();
+
+            System.out.println(response.toXml());
+            return response.toXml();
+
+        } catch (TwiMLException e) {
+            e.printStackTrace();
+        }
+
+        return "";
     }
 
 
