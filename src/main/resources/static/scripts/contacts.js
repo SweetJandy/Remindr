f42a72c60366914d9181c181737b07733d3c6560
brandongossen <brandongossen@yahoo.com>
2017-11-21 23:59:07 -0600

    Remindr JSON Ajax Request
    -Create the file contacts.js that uses the JSON response from the client side with an AJAX request using jQuery.
    
    -Create an html file called ajax.html that will only load an empty div, but will call the post.js file to formulate the data from the Ajax request into html.

---
+++ src/main/resources/static/scripts/contacts.js
@@ -0,0 +1,13 @@
+(function($) {
+    var request = $.ajax({'url': '/contacts.json'});
+    request.done(function (contacts) {
+        var html = '';
+        contacts.forEach(function(contact) {
+            html += '<div class="contact">';
+            html += '<h2 id="contact-name">' + contact.firstName + " " + contact.lastName + '</h2>';
+            html += '<h4 id="contact-number">' + contact.phoneNumber + '</h4>';
+            html += '</div>';
+        });
+        $('#contacts').html(html);
+    });
+})(jQuery);
