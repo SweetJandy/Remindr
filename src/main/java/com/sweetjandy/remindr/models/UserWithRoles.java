cf20beab145ea0b14adac472540060a1425f2bc7
brandongossen <brandongossen@yahoo.com>
2017-11-26 23:12:36 -0600

    Remindr Security Authenticate and Authorize Users 2nd commit
    -Add the spring-boot-starter-security and thymeleaf-extras-springsecurity4 dependencies node into the pom.xml file.
    
    -Create a model called UsersWithRoles that implements the UserDetails and input the information required to authenticate and authorize users.

--- src/main/java/com/sweetjandy/remindr/models/UserWithRoles.java
+++ src/main/java/com/sweetjandy/remindr/models/UserWithRoles.java
@@ -4,7 +4,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.util.StringUtils;
-
 import java.util.Collection;
 import java.util.List;
 
