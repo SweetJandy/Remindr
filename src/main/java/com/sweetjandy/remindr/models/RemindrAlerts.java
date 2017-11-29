cb3229b62678a4ee4649ee4365568d7295f0b8dd
amyyan210 <amyyan210@gmail.com>
2017-11-29 14:59:17 -0600

    Edit alerts on remindr

--- src/main/java/com/sweetjandy/remindr/models/RemindrAlerts.java
+++ src/main/java/com/sweetjandy/remindr/models/RemindrAlerts.java
@@ -1,8 +1,65 @@
 package com.sweetjandy.remindr.models;
 
+import java.util.List;
+
 public class RemindrAlerts {
     private String alertTimes;
     private Long id;
+    private List<Alert> alertList;
+    public boolean ZERO;
+    public boolean FIFTEEN;
+    public boolean THIRTY;
+    public boolean HOUR;
+    public boolean DAY;
+    public boolean WEEK;
+
+    public boolean isZERO() {
+        return ZERO;
+    }
+
+    public void setZERO(boolean ZERO) {
+        this.ZERO = ZERO;
+    }
+
+    public boolean isFIFTEEN() {
+        return FIFTEEN;
+    }
+
+    public void setFIFTEEN(boolean FIFTEEN) {
+        this.FIFTEEN = FIFTEEN;
+    }
+
+    public boolean isTHIRTY() {
+        return THIRTY;
+    }
+
+    public void setTHIRTY(boolean THIRTY) {
+        this.THIRTY = THIRTY;
+    }
+
+    public boolean isHOUR() {
+        return HOUR;
+    }
+
+    public void setHOUR(boolean HOUR) {
+        this.HOUR = HOUR;
+    }
+
+    public boolean isDAY() {
+        return DAY;
+    }
+
+    public void setDAY(boolean DAY) {
+        this.DAY = DAY;
+    }
+
+    public boolean isWEEK() {
+        return WEEK;
+    }
+
+    public void setWEEK(boolean WEEK) {
+        this.WEEK = WEEK;
+    }
 
     public Long getId() {
         return id;
@@ -18,6 +75,14 @@ public class RemindrAlerts {
 
     public void setAlertTimes(String alertTimes) {
         this.alertTimes = alertTimes;
+    }
+
+    public List<Alert> getAlertList() {
+        return alertList;
+    }
+
+    public void setAlertList(List<Alert> alertList) {
+        this.alertList = alertList;
     }
 }
 
