97813a8f774b61d5ec622fd625dd16f18546c215
brandongossen <brandongossen@yahoo.com>
2017-12-01 10:15:35 -0600

    Make sure deleting a remindr will not delete the contact assoicated with that remindr
    -In the Contact model switch the cascadeType from all to Detach so when deleting a remindr it detaches all related entities from being removed as well.
    
    -Add a @PathVariable to the edit contact method that tracks the full contact object and it's properties to make sure when contacts are edited no properties become null in the database.

--- src/main/java/com/sweetjandy/remindr/models/Remindr.java
+++ src/main/java/com/sweetjandy/remindr/models/Remindr.java
@@ -61,7 +61,7 @@ public class Remindr {
     @JsonManagedReference
     private User user;
 
-    @ManyToMany(cascade = CascadeType.ALL)@JoinTable
+    @ManyToMany(cascade = CascadeType.DETACH)@JoinTable
             (name = "contact_remindr",
                     joinColumns = {@JoinColumn(name = "remindr_id")},
                     inverseJoinColumns = {@JoinColumn(name = "contact_id")}
